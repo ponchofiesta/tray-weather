3e7393277419bb0abdf384921754dd8256cc0190
Michael Richter <mr@osor.de>
2024-10-04 13:59:55 +0200

    Nicer GUI layout

--- src/gui.rs
+++ src/gui.rs
@@ -3,7 +3,7 @@ use std::{
     sync::mpsc::{channel, Receiver, Sender},
 };
 
-use eframe::egui::{self, Label, TextEdit, Ui};
+use eframe::egui::{self, Checkbox, TextEdit, Ui};
 use log::debug;
 use reqwest::Url;
 use rust_i18n::t;
@@ -129,39 +129,42 @@ impl SettingsWindow {
 
 impl SettingsWindow {
     fn settings_screen(&mut self, ctx: &egui::Context, ui: &mut Ui) {
-        ui.horizontal(|ui| {
-            ui.vertical(|ui| {
-                let location_label = ui.label(t!("location"));
-                ui.add(Label::new(self.location.to_human_readable()))
-                    .labelled_by(location_label.id);
+        ui.heading(t!("settings_heading"));
 
-                if ui.button(t!("new_location")).clicked() {
-                    self.screen = SettingsScreen::Location;
+        setting_entry(ui, t!("location"), |ui| {
+            let text = if self.location.id != 0 {
+                self.location.to_human_readable()
+            } else {
+                t!("empty_location").into()
+            };
+            if ui.button(text).clicked() {
+                self.screen = SettingsScreen::Location;
+            }
+        });
+
+        setting_entry(ui, t!("autostart", name = PROGRAM_NAME), |ui| {
+            ui.add(Checkbox::without_text(&mut self.autorun_enabled));
+        });
+
+        ui.horizontal(|ui| {
+            ui.with_layout(egui::Layout::right_to_left(egui::Align::BOTTOM), |ui| {
+                // reversed because of right_to_left
+                let cancel_button = ui.button(t!("dialog.cancel"));
+                let save_button = ui.button(t!("dialog.save"));
+
+                if save_button.clicked() {
+                    if let Some(tx) = &self.tx_window {
+                        let settings = Settings {
+                            location: self.location.clone(),
+                            autorun_enabled: self.autorun_enabled,
+                        };
+                        tx.send(Some(settings)).unwrap();
+                    }
+                    self.close_window(ctx);
+                } else if cancel_button.clicked() {
+                    self.close_window(ctx);
                 }
             });
-        });
-        ui.horizontal(|ui| {
-            ui.checkbox(
-                &mut self.autorun_enabled,
-                t!("autostart", name = PROGRAM_NAME),
-            );
-        });
-        ui.horizontal(|ui| {
-            let save_button = ui.button(t!("dialog.save"));
-            let cancel_button = ui.button(t!("dialog.cancel"));
-
-            if save_button.clicked() {
-                if let Some(tx) = &self.tx_window {
-                    let settings = Settings {
-                        location: self.location.clone(),
-                        autorun_enabled: self.autorun_enabled,
-                    };
-                    tx.send(Some(settings)).unwrap();
-                }
-                self.close_window(ctx);
-            } else if cancel_button.clicked() {
-                self.close_window(ctx);
-            }
         });
     }
 
@@ -177,26 +180,29 @@ impl SettingsWindow {
             None => (),
         };
 
+        ui.heading(t!("location_heading"));
+
         ui.horizontal(|ui| {
-            ui.vertical(|ui| {
                 let location_label = ui.label(t!("location"));
+
                 ui.add(TextEdit::singleline(&mut self.location_name).desired_width(80.0))
                     .labelled_by(location_label.id);
-            });
+
+                if ui.button(t!("search_location")).clicked() {
+                    let name: String = self.location_name.clone();
+                    let tx = match &self.tx_locations {
+                        Some(tx) => tx.clone(),
+                        None => panic!(),
+                    };
+                    tokio::spawn(async move {
+                        let results = search_location(&name, "de").await;
+                        tx.send(results).unwrap();
+                    });
+                }
         });
-        ui.horizontal(|ui| {
-            if ui.button(t!("search_location")).clicked() {
-                let name: String = self.location_name.clone();
-                let tx = match &self.tx_locations {
-                    Some(tx) => tx.clone(),
-                    None => panic!(),
-                };
-                tokio::spawn(async move {
-                    let results = search_location(&name, "de").await;
-                    tx.send(results).unwrap();
-                });
-            }
-        });
+
+        ui.separator();
+
         ui.horizontal(|ui| {
             if let Some(locations) = &self.found_locations {
                 ui.vertical(|ui| {
@@ -214,7 +220,16 @@ impl SettingsWindow {
 
 impl eframe::App for SettingsWindow {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
+        // Styling
+        let mut style: egui::Style = (*ctx.style()).clone();
+        style.spacing.button_padding = egui::vec2(12.0, 8.0);
+        style.spacing.item_spacing = egui::vec2(16.0, 8.0);
+        ctx.set_style(style.clone());
+        let frame = egui::Frame::none()
+            .inner_margin(egui::Margin::same(16.0))
+            .fill(style.visuals.panel_fill);
+
+        egui::CentralPanel::default().frame(frame).show(ctx, |ui| {
             match self.screen {
                 SettingsScreen::Home => self.settings_screen(ctx, ui),
                 SettingsScreen::Location => self.location_screen(ui),
@@ -223,12 +238,36 @@ impl eframe::App for SettingsWindow {
     }
 }
 
+fn setting_entry<R>(
+    ui: &mut Ui,
+    label: impl Into<egui::WidgetText>,
+    add_contents: impl FnOnce(&mut Ui) -> R,
+) {
+    egui::Frame::default()
+        .fill(egui::Color32::from_rgb(250, 250, 250))
+        .stroke(egui::Stroke::new(
+            0.5,
+            egui::Color32::from_rgb(220, 220, 220),
+        ))
+        .rounding(egui::Rounding::same(4.0))
+        .inner_margin(egui::Margin::symmetric(16.0, 20.0))
+        .show(ui, |ui| {
+            ui.horizontal(|ui| {
+                ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui| {
+                    ui.label(label);
+                });
+                ui.add_space(ui.available_width());
+                ui.with_layout(egui::Layout::right_to_left(egui::Align::TOP), add_contents);
+            });
+        });
+}
+
 pub(crate) fn show_settings_window(settings: &Settings) -> Option<Settings> {
     let (tx, rx) = channel::<Option<Settings>>();
     let settings_window = SettingsWindow::new(tx.clone(), settings);
 
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([480.0, 320.0]),
         ..Default::default()
     };
     eframe::run_native(
