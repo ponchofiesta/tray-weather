f396b2184d4f8d14212836226667f2b4b83d9fb5
Michael Richter <mr@osor.de>
2024-12-11 12:25:06 +0100

    update build-resource

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 fn main() {
     // Add ICO resources
-    embed_resource::compile("assets/resources.rc", embed_resource::NONE);
+    embed_resource::compile("assets/resources.rc", embed_resource::NONE)
+        .manifest_optional()
+        .unwrap();
     println!("cargo:rustc-link-arg-bin=tray-weather=/RES:assets/resources.res");
 }
